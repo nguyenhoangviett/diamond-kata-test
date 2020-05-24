32ee4344d080835d549020ea929198c016faf6fd
nguyenhoangviett <62774513+nguyenhoangviett@users.noreply.github.com>
2020-05-24 19:13:00 +0200

    Add files via upload

--- diamond-kata.test.js
+++ diamond-kata.test.js
@@ -1,6 +1,29 @@
-const{diamondKata}=require("./diamond-kata")
-describe("diamond-kata", ()=> {
-    it ("should return zero", ()=> {
-        expect(diamondKata()).toBe(0)
-    })
+const { printDiamond } = require('./diamond-kata')
+
+test('should print A diamond', () => {
+    expect(printDiamond("A")).toBe("A")
 })
+
+test('should print B diamond', () => {
+    expect(printDiamond("B")).toBe(" A\n" +
+                                   "B B\n" +
+                                   " A")
+})
+
+test('should print C diamond', () => {
+    expect(printDiamond("C")).toBe("  A\n" +
+                                   " B B\n" +
+                                   "C   C\n" +
+                                   " B B\n" +
+                                   "  A")
+}) 
+
+test('should print D diamond', () => {
+    expect(printDiamond("D")).toBe("   A\n" +
+                                   "  B B\n" +
+                                   " C   C\n" +
+                                   "D     D\n" +
+                                   " C   C\n" +
+                                   "  B B\n" +
+                                   "   A")
+}) 
